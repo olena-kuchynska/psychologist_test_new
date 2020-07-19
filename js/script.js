dc9ef4aceca35eafbbb6af080378d4fedd07314b
Olena-Bulatova <bulatova853@gmail.com>
2020-07-19 17:24:40 +0300

    Add heder block

--- js/script.js
+++ js/script.js
@@ -4,32 +4,65 @@ class AppView {
     }
 
     showForm() {
-        const container = document.querySelector('.container');
+        const container = document.querySelector('.wrapper');
 
         container.innerHTML = 
-            `<div class="header">
-                <div class="nav">
+            `<header class="header">        
+                <!--<input id="burger" type="checkbox" class="header__burger-state"/>                
+                <label for="burger" class="header__burger-btn">
+                    <span class="header__burger-icon1"></span>
+                    <span class="header__burger-icon2"></span>
+                    <span class="header__burger-icon3"></span>
+                </label>-->
+                <nav> 
+                    <ul class="header-nav">
+                        <li class="header-nav__item">
+                            <a href="#about" class="header-nav__link">Обо мне</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a href="" class="header-nav__link">Консультации</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a href="" class="header-nav__link">Кабинет</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a href="#recording" class="header-nav__link">Записаться</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <article class="article">
+            <div class="article__photo">
+            </div>
+            <div class="article__info">
+                <p class="article__info-title">Татьяна Колесник</p>
+                <p class="article__info-subtitle">Психолог, психотерапевт</p>
+                <p class="article__info-text">
+                Я помогаю улучшить качество жизни и конструктивно решать проблемы,
+                от конфликтов в семье до вопросов саморазвития.
+                </p>
+            </div>
+            </article>
+            <main class="mainContent">
+                <div class="principles" id="principles">
 
                 </div>
-            </div>
-            <div class="principles">
+                <div class="questions" id="questions">
 
-            </div>
-            <div class="questions">
+                </div>
+                <div class="about" id="about">
 
-            </div>
-            <div class="about">
+                </div>
+                <div class="education" id="education">
 
-            </div>
-            <div class="education">
+                </div>
+                <div class="recording" id="recording">
 
-            </div>
-            <div class="recording">
+                </div>
+            </main>            
+            <footer class="footer">
 
-            </div>
-            <div class="footer">
-
-            </div>`;
+            </footer>`;
     }
 }
 
