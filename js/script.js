01656ef53522957e83dbdee80cec5d75cf34f056
Olena-Bulatova <bulatova853@gmail.com>
2020-09-13 00:18:58 +0300

    Add adeptive ro feedbakc

--- js/script.js
+++ js/script.js
@@ -413,7 +413,7 @@ class AboutFormControll {
         const recording = document.querySelector('.recording');                
         const footer = document.querySelector('.footer');
 
-        burger.addEventListener('click', event => {            
+        burger.addEventListener('change', event => {            
             
 
             if (event.target.checked === true) {                                
@@ -440,6 +440,7 @@ class AboutFormControll {
             const body = document.querySelector('body');
             body.removeAttribute('style');
             burger.checked = false;
+            
             article.classList.remove('blur');
             mainContent.classList.remove('blur');
             footer.classList.remove('blur');
@@ -551,7 +552,8 @@ class ConsultationView {
                 <div class="article-new">
                     <p class="article__info-title">Консультации</p>
                     <p class="article__info-subtitle article__info-subtitle--italic">Моя основная специализация&nbsp;&mdash; отношения.</p>
-                    <img class="tree tree__first--consult" src="./images/tree-3.png" alt="tree" />
+                    <div class="article-new__tree"></div>
+                    <!--<img class="tree tree__first--consult" src="./images/tree-3.png" alt="tree" />-->
                 </div>
                     <div class="relationship__image">
                         <div class="relationship__image-block"></div>
@@ -663,7 +665,7 @@ class ConsultationView {
                 <div class="answer__block-text">Сразу же после первого сеанса с Татьяной я вышла с широкой улыбкой на лице. Проблемы превратились в решаемые задачи и абсолютно перестали беспокоить, как это было раньше. Очень приятный и светлый человек. Татьяна, огромное Вам спасибо!</div>                           
                 <span class="answer__block-author">Татьяна Колесник</span>
             </div>
-            <div class="answer__block">
+            <div class="answer__block answer__block--even">
                 <img class="tree tree__second--feedbacks" src="./images/tree-3.png" alt="tree" />
                 <div class="answer__block-text answer__block-text--even">Пришла к Татьяне в полном состоянии не ресурса и апатии. Было чувство, что запуталась в жизни, понимания "Кто я?" и "Чем хочу заниматься". Всего несколько встреч с Ириной помогли вернутся в ресурсное состоянии и желания творить. Больше всего понравилось, что все индивидуально и работа идет по моему запросу. Я люблю решать проблемы быстро и Татьянаподкупила тем, что не надо будет ходить 1,5 года на терапию чтобы решить мою задачу. Хотя другие специалисты предлагали свои услуги от 10 встреч и более :) Татьяна, огромная благодарность за профессионализм, индивидуальны подход и кропотливый труд в нашей совместной работе. Не всегда было легко, но однозначно этого того стоило. Я теперь точно знаю к кому идти, если на жизненном пути возникли трудности. И вам рекомендую.</div>                      
                 <span class="answer__block-author answer__block-author--even">Татьяна Колесник</span></div>                          
@@ -671,7 +673,7 @@ class ConsultationView {
                 <div class="answer__block-text">Мені достатньо було одного візиту до Татьяни ,щоб зрозуміти всю суть моєї проблеми,і дійсно зараз я дуже щаслива,і кохана людина,тим паче і сама кохаю, спілкування було дуже приємним, і я змогла повністю розкритися психологу.Хочу вас подякувати Ірино,за те що ви є,і допомагаєте людям,іноді сам ти не взмозі зрозуміти ,і друзі і рідні теж не можуть тобі відкрити очі і допомогти ,тому звернення до психолога це вихід,тим більш допомога собі розібратися в свої думках,які дуже швидку змінюються.Ви мені дуже допомогли ,я дуже щаслива.Дякую.</div>                           
                 <span class="answer__block-author">Татьяна Колесник</span></div>                          
             </div> 
-            <div class="answer__block">
+            <div class="answer__block answer__block--even">
                 <img class="tree tree__third--feedbacks" src="./images/tree-5.png" alt="tree" />
                 <div class="answer__block-text answer__block-text--even">Хочу поблагодарить Татьяну за помощь! Мне очень помогли консультации. Я осознала некоторые очевидные вещи в своей жизни, о которых раньше даже не задумывалась. Смогла изменить отношение к проблемам из прошлого, которые до сих пор меня беспокоили, и, что самое важное, переосмыслить свое будущее.</div>                           
                 <span class="answer__block-author answer__block-author--even">Татьяна Колесник</span></div>                          
@@ -681,7 +683,7 @@ class ConsultationView {
                 <div class="answer__block-text">Обращалась по поводу проблем после того, как переболела серьезной болезнью и лежала в больнице. Хотя у меня есть не одна подруга, рассказывать им о своих проблемах было сложно. Да и что они могли посоветовать - "не парься и все пройдет". После нескольких сеансов поняла для себя, что все проблемы, которые у меня есть находятся у меня внутри и справиться с ними могу я сама. И это было как-то удивительно - вроде человек тебе помогает, но и вроде как ты помогаешь себе сама. Я очень благодарна и сейчас прохожу терапию у Татьяны!</div>                           
                 <span class="answer__block-author">Татьяна Колесник</span></div>
             </div> 
-            <div class="answer__block">
+            <div class="answer__block answer__block--even">
                 <div class="answer__block-text answer__block-text--even">Моя история прихода к психологу Татьяне, начинается с того ,что я запутался сам в себе. Я как бы знал что мне делать и в тоже время не знал... Наверное эта ситуация знакома многим. Буду краток, мне кажется что каждый из нас нуждается во взгляде на наш внутренний мир, и лучше когда это делает профессионал своего дела. Огромное спасибо за профессиональную консультацию. С признательностью и уважением Юра. =))</div>                           
                 <span class="answer__block-author answer__block-author--even">Татьяна Колесник</span></div>                          
             </div> 
